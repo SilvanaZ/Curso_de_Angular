bdfec43940fccf4b4ae4cccdf8eb634cba6d7957
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-09-03 22:36:03 -0300

    Aññadiendo ArregloTarjetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface Tarjeta {
+  titulo: string;
+  subtitulo: string;
+  image?: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'FirstApp';
+export class AppComponent implements OnInit {
+  title = 'Mi app de Angular';
+  public ArregloTarjetas: Tarjeta[] = [];
+
+  ngOnInit(): void {
+    this.ArregloTarjetas = [
+      {
+        titulo: 'Video 1',
+        subtitulo: 'Subtitulo 1',
+        image:
+          'https://i.ytimg.com/vi/PSD5pFi6bx4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBRXrJxTLALTsBas1djapMjtW0lQg',
+      },
+      {
+        titulo: 'Video 2',
+        subtitulo: 'Subtitulo 2',
+        image:
+          'https://i.ytimg.com/vi/rOsd3OI9dOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdMDLcELCLzlSzVoELr-QZ41mL8g',
+      },
+      {
+        titulo: 'Video 3',
+        subtitulo: 'Subtitulo 3',
+        image:
+          'https://i.ytimg.com/vi/lasGrvRYBWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDgh8fipV-k2f4xjEEPpvvtiWHwjA',
+      },
+    ];
+  }
 }
